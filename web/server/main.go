6501386dcad924731c76251733967cb65bcf95d0
Anthony Freda <anthonyfreda323@gmail.com>
2018-12-17 22:13:10 -0500

    implemented simple web server

---
+++ web/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Suh dood")
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Error starting server", err)
+	}
+}
