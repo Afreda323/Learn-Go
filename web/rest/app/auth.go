7e94c83382d44a6909886dd3441a92e032e380bf
Anthony Freda <anthonyfreda323@gmail.com>
2018-12-19 10:56:12 -0500

    added jwt middleware

---
+++ web/rest/app/auth.go
@@ -0,0 +1,73 @@
+package app
+
+import (
+	u "Learn-Go/web/rest/utils"
+	"context"
+	"net/http"
+	"os"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func JwtAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		unauthPages := []string{"/api/user/new", "/api/user/login"}
+		requestPath := r.URL.Path
+
+		// check to see if hitting an unauthed route
+		// if so, move along
+		for _, path := range unauthPages {
+			if path == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		response := make(map[string]interface{})
+		token := r.Header.Get("Authorization") // get token header
+
+		// if token is empty, send error message
+		if token == "" {
+			response = u.Message(false, "Auth token not provided")
+			w.WriteHeader(http.StatusForbidden)
+			u.Respond(w, response)
+			return
+		}
+
+		splitToken := strings.Split(token, " ") // split token into ["Bearer", "token"]
+
+		if len(splitToken) != 2 {
+			response = u.Message(false, "Invalid/Malformed token provided")
+			w.WriteHeader(http.StatusForbidden)
+			u.Respond(w, response)
+			return
+		}
+
+		tokenVal := splitToken[1]
+		tk := &models.token
+
+		token, err := jwt.ParseWithClaims(tokenVal, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("TOKEN_SECRET")), nil
+		})
+
+		if err != nil {
+			response = u.Message(false, "Invalid/Malformed token provided")
+			w.WriteHeader(http.StatusForbidden)
+			u.Respond(w, response)
+			return
+		}
+
+		if !token.Valid {
+			response = u.Message(false, "Invalid/Malformed token provided")
+			w.WriteHeader(http.StatusForbidden)
+			u.Respond(w, response)
+			return
+		}
+
+		// Everything went well
+		ctx := context.WithValue(r.Context(), "user", tk.userId)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
